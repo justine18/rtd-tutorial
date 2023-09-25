0a27fda3a2df1357e8f9e81ad1b5d3072c4ae4a1
Justine Broihan <jbroihan@gams.com>
2023-09-25 08:47:38 +0200

    replace py file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'GAMSPy Test'
+copyright = '2023, Justine'
+author = 'Justine'
 
 release = '0.1'
 version = '0.1.0'
